2a12c2c93a4381f9d371926d3e2b226163dc3810
egor975 <116854135+egor975@users.noreply.github.com>
2023-04-02 11:14:59 +0000

    Initial commit

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const games = [
     link: "https://ant-agl.github.io/other/tic-tac-toe/",
   },
   {
-    id: "sapper",
+    id: "Ya1",
     name: "Hunter Assassin",
+    link: "https://yandex.ru/games/app/168142",
+  },
+  {
+    id: "Ya2",
+    name: "Добавить игру",
     link: "https://yandex.ru/games/app/168142",
   },
   //   {
