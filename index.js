51fabe4dc2aaa54225c222768db8c7c2b247ee81
ant-agl <aglanton31@gmail.com>
2023-04-02 21:32:28 +0300

    test

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const start = () => {
     if (text === "/play") {
       return bot.sendSticker(
         chatId,
-        "CAACAgIAAxkBAAEIbfxkKcRZW8WNBj5D4T3pqNUKxuSL-QACnywAAsm2UUlNG7jdAAG11vwvBA"
+        "CAACAgIAAxkBAAEIbfxkKcRZW8WNBj5D4T3pqNUKxuSL"
       );
       return bot.sendMessage(
         chatId,
